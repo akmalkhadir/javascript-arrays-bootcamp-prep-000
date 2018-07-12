fdff9178e0c8e57eff2db739c3cfdb2c5e1da37b
Akmal Khadir <akmal.a.khadir@gmail.com>
2018-07-12 16:03:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,3 +5,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray (array,element){
   return [element, ...array]
 }
+
+function addElementToBeginningOfArray (array,element){
+  return [element, ...array]
+}
